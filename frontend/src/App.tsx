14b5e2bf4929b2a3221f14b4740ba2bc461f5bb6
Madushanka Wakwalle <madushanka88online@gmail.com>
2025-11-27 02:09:59 +0530

    Addes Welcome to the Microstore in the Heading

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -32,7 +32,7 @@ function App() {
   return (
     <div className="App">
       <nav className="navbar">
-        <h1>MicroStore 🚀 v1.0</h1>
+        <h1>MicroStore 🚀 v1.0 - Welcome to the Microstore</h1>
       </nav>
 
       <div className="container">
