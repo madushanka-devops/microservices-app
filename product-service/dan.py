a4ee543520ea81365acdbd3a326399d663e51ed3
Madushanka Wakwalle <madushanka88online@gmail.com>
2025-12-12 16:40:46 +0530

    Test: Triggering a vulnerable code in the Product Service

---
+++ product-service/dan.py
@@ -0,0 +1,6 @@
+import subprocess
+
+def run_command(user_input):
+    # This is a Command Injection vulnerability!
+    # Semgrep p/default will catch "shell=True" with variable input
+    subprocess.call(user_input, shell=True)
