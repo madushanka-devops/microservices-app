1a512813b967acf12f0a473f08796bab5153a850
Madushanka Wakwalle <madushanka88online@gmail.com>
2025-12-21 04:04:45 +0530

    Removing 3rd party dependency to trigger Software Composotion Analysis with Trivy

--- product-service/dan.py
+++ product-service/dan.py
@@ -1,6 +1,6 @@
-import subprocess
+# import subprocess
 
-def run_command(user_input):
-   # This is a Command Injection vulnerability!
-   # Semgrep p/default will catch "shell=True" with variable input
-   subprocess.call(user_input, shell=True)
+# def run_command(user_input):
+#    # This is a Command Injection vulnerability!
+#    # Semgrep p/default will catch "shell=True" with variable input
+#    subprocess.call(user_input, shell=True)
